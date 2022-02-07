ef203cfe2b13659da44421310cb2ea1ca90310ff
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-02-07 11:22:22 +0000

    better tables and plots for paper

--- scripts/paper_main_BUGS_tables.R
+++ scripts/paper_main_BUGS_tables.R
@@ -1,24 +1,23 @@
 
-# tables
+# tables for paper
 
 
 library(MCMCvis)
+library(dplyr)
 
-load("data/BUGS_output.RData")
+
+load(here::here("input_data/BUGS_output.RData"))
 
 num_settings <- 5
 mcmc_dat <- res_bugs$BUGSoutput$sims.list
 
-######################
-# population
 
-tab_pop <- 
+############################################
+# population counts per incident
+# for each subgroup
+
+tab_pop_inc <- 
   cbind(
-    transmute(MCMCsummary(
-      res_bugs,
-      params = "srate_inc",
-      round = 3)[, c("mean", "sd")],
-      n_incidents = paste0(mean, " (", sd, ")")),
     transmute(MCMCsummary(
       res_bugs,
       params = "srate_id",
@@ -45,12 +44,37 @@ tab_pop <-
       round = 3)[, c("mean", "sd")],
       p_ltbi = paste0(mean, " (", sd, ")")))
 
-# MCMCtrace(res_bugs, 
-#           ISB = FALSE, 
-#           exact = TRUE,
-#           pdf = FALSE)
+write.csv(tab_pop_inc, file = here::here("output_data/pop_summary_table_per_inc.csv"))
 
-write.csv(tab_pop, file = "output_data/pop_summary_table.csv")
+
+############################################
+# population counts per year
+# for each subgroup
+
+##TODO: not strictly correct because should sample srate_inc number of number id
+##      at the moment we use the same number id
+
+mean_id <- apply(mcmc_dat$srate_inc * mcmc_dat$srate_id, 2, mean)
+sd_id <- apply(mcmc_dat$srate_inc * mcmc_dat$srate_id, 2, sd)
+mean_screen <- apply(mcmc_dat$srate_inc * mcmc_dat$pred_n_screen, 2, mean)
+sd_screen <- apply(mcmc_dat$srate_inc * mcmc_dat$pred_n_screen, 2, sd)
+mean_ltbi <- apply(mcmc_dat$srate_inc * mcmc_dat$pred_n_ltbi, 2, mean)
+sd_ltbi <- apply(mcmc_dat$srate_inc * mcmc_dat$pred_n_ltbi, 2, sd)
+
+tab_pop <- 
+  cbind(
+    transmute(MCMCsummary(
+      res_bugs,
+      params = "srate_inc",
+      round = 3)[, c("mean", "sd")],
+      n_incidents = paste0(mean, " (", sd, ")")),
+    n_identify = paste0(round(mean_id,3), " (", round(sd_id,3), ")"),
+    n_screen = paste0(round(mean_screen,3), " (", round(sd_screen,3), ")"),
+    n_screen = paste0(round(mean_ltbi,3), " (", round(sd_ltbi,3), ")")
+    )
+
+write.csv(tab_pop, file = here::here("output_data/pop_summary_table.csv"))
+
 
 ###################
 # costs
@@ -68,5 +92,5 @@ tab_cost <-
     pp_ltbi = paste0(round(apply(c_dat/mcmc_dat$pred_n_ltbi, 2, mean),0), " (",
                      round(apply(c_dat/mcmc_dat$pred_n_ltbi, 2, sd),0), ")"))
 
-write.csv(tab_cost, file = "output_data/cost_summary_table.csv")
+write.csv(tab_cost, file = here::here("output_data/cost_summary_table.csv"))
 
